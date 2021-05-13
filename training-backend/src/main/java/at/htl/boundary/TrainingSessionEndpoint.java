034da3f11286e9ee3678021bfb4697c23c9d6a15
simonittensammer <simon.ittensammer@gmx.at>
2021-05-13 22:28:26 +0200

    add findByIdUsingQueryParam in TrainingSessionEndpoint

--- training-backend/src/main/java/at/htl/boundary/TrainingSessionEndpoint.java
+++ training-backend/src/main/java/at/htl/boundary/TrainingSessionEndpoint.java
@@ -4,10 +4,7 @@ import at.htl.control.TrainingSessionRepository;
 import at.htl.entity.TrainingSession;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -28,6 +25,12 @@ public class TrainingSessionEndpoint {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response findById(@PathParam("id") Long id) {
+        return Response.ok(tsr.findById(id)).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findByIdUsingQueryParam(@QueryParam("id") Long id) {
         return Response.ok(tsr.findById(id)).build();
     }
 }
