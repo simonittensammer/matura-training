1c199ab04c17861c2651ee20752d44b15bbb86b2
simonittensammer <simon.ittensammer@gmx.at>
2021-05-14 15:12:48 +0200

    add endpoints for athlete and coach

--- training-backend/src/main/java/at/htl/boundary/AthleteEndpoint.java
+++ training-backend/src/main/java/at/htl/boundary/AthleteEndpoint.java
@@ -1,4 +1,23 @@
 package at.htl.boundary;
 
+import at.htl.control.AthleteRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/athlete")
 public class AthleteEndpoint {
+
+    @Inject
+    AthleteRepository ar;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAll() {
+        return Response.ok(ar.listAll()).build();
+    }
 }
--- training-backend/src/main/java/at/htl/boundary/CoachEndpoint.java
+++ training-backend/src/main/java/at/htl/boundary/CoachEndpoint.java
@@ -1,4 +1,23 @@
 package at.htl.boundary;
 
+import at.htl.control.CoachRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/coach")
 public class CoachEndpoint {
+
+    @Inject
+    CoachRepository cr;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAll() {
+        return Response.ok(cr.listAll()).build();
+    }
 }
